b66a916cb89d0fa135e18c5c28d27e7f9f12bcd5
Aman katiyar <97542315+amankatiyar321@users.noreply.github.com>
2022-05-20 23:51:12 +0530

    Add files via upload

---
+++ Resturent-Detailes/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ResDet from './components/ResDet';
+
+function App() {
+  return (
+    <div className="App">
+      <ResDet/>
+    
+    </div>
+  );
+}
+
+export default App;
